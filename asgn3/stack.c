f0fe9117323b6ff34a82059c29d52c095725528d
Govind Pillai <gopillai@ucsc.edu>
2021-04-25 02:02:47 -0700

    implementing stack functions

--- asgn3/stack.c
+++ asgn3/stack.c
@@ -1,6 +1,10 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include "stack.h"
+#include <stdlib.h>
+#include <inttypes.h>
+
+
 
 typedef struct Stack Stack {
 	uint32_t top;
@@ -8,17 +12,63 @@ typedef struct Stack Stack {
 	int64_t *items;
 } Stack;
 
-Stack *stack_create(uint32_t capacity)
+Stack *stack_create(uint32_t capacity) {
+	Stack *s = (Stack *)malloc(sizeof(Stack));
+	if(s != NULL) {
+		s->top = 0;
+		s->capacity = capacity;
+		s->items = (int64_t *)calloc(capacity, sizeof(int64_t));
+		if(s == NULL) {
+			free(s);
+			s = NULL;
+		}
+	}
+	return s;
+}
 
-void stack_delete(Stack **s);
+void stack_delete(Stack **s) {
+	if(*s && (*s)->items) {
+		free((*s)->items);
+		free(*s);
+		*s = NULL;
+	}
+	return;
+}
 
-bool stack_empty(Stack *s);
+bool stack_empty(Stack *s) {
+	return s->top == 0;
+}
 
-uint32_t stack_size(Stack *s);
+bool stack_full(Stack *s) {
+	return s->top == s->capacity;
+}
 
-bool stack_push(Stack *s, int64_t x);
+uint32_t stack_size(Stack *s) {
+	return s->top;
+}
 
-bool stack_pop(Stack *s, int64_t *x);
+bool stack_push(Stack *s, int64_t x) {
+	if(stack_full(s)) {
+		return false;
+	}
+	s->items[s->top] = x;
+	s->top += 1;
+	return true;
+}
 
-void stack_print(Stack *s);
+bool stack_pop(Stack *s, int64_t *x) {
+	if(stack_empty(s)) {
+		return false;
+	}
+	s->top -= 1;
+	*x = s->items[s->top];
+	return true;
+}
+void stack_print(Stack *s) {
+	printf("[");
+	for(uint32_t i = 0; i < s->top; i++) {
+		printf("%" PRId64, s->items[i]);
+		printf(", ");
+	}
+}
 
