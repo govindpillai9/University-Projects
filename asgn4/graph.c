6c983626f2ca09211e97523413e230cdb8e1d5f3
Govind Pillai <gopillai@ucsc.edu>
2021-05-01 01:59:46 -0700

    implemented graph.c

---
+++ asgn4/graph.c
@@ -0,0 +1,77 @@
+#include "graph.h"
+
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Graph Graph {
+	uint32_t vertices;
+	bool undirected;
+	bool visited[VERTICES];
+	uint32_t matrix[VERTICES][VERTICES];
+}
+
+Graph *graph_create(uint32_t vertices, bool undirected) {
+	Graph *g = (Graph *) malloc(sizeof(Graph));
+	g->vertices = vertices;
+	g->undirected = undirected;
+	for(uint32_t i = 0; i < VERTICES; i++) {
+		g->visited[i] = false;
+	}
+	for(uint32_t x = 0; x < VERTICES; x++) {
+		for(uint32_t y = 0; y < VERTICES; y++) {
+			g->matrix[x][y] = 0;
+		}
+	}
+}
+
+void graph_delete(Graph **G) {
+	if (*G) {
+        	free(*G);
+        	*G = NULL;
+    	}
+    	return;
+
+}
+
+uint32_t graph_vertices(Graph *G) {
+	return g->vertices;
+}
+
+bool graph_add_edge(Graph *G, uint32_t i, uint32_t j, uint32_t k) {
+	g->matrix[i][j] = k;
+}
+
+bool graph_has_edge(Graph *G, uint32_t i, uint32_t j) {
+	return g->matrix[i][j] != 0;
+}
+
+uint32_t graph_edge_weight(Graph *G, uint32_t i, uint32_t j) {
+	return g->matrix[i][j];
+}
+
+bool graph_visited(Graph *G, uint32_t v) {
+	return g->visited[v];
+}
+
+void graph_mark_visited(Graph *G, uint32_t v) {
+	g->visited[v] = true;
+}
+void graph_mark_unvisited(Graph *G, uint32_t v) {
+	g->visited[v] = false;
+}
+
+void graph_print(Graph *G) {
+	for(uint32_t x = 0; x < VERTICES; x++) {
+		for(uint32_t y = 0; y < VERTICES; y++) {
+                        printf("%d ",g->matrix[x][y]);
+			if(y == VERTICES - 1) {
+				printf("\n");
+			}
+                }
+        }
+
+}
+
