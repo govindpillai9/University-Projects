6732cdcb3dc115010ed668d4a8fa71e7707d0e67
Govind Pillai <gopillai@ucsc.edu>
2021-05-22 01:06:26 -0700

    creating node and priority queue

---
+++ asgn6/node.c
@@ -0,0 +1,51 @@
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "node.h"
+Node *node_create(uint8_t symbol, uint64_t frequency) {
+    Node *n = (Node *) malloc(sizeof(Node));
+    if (n) {
+        n->symbol = symbol;
+	n->frequency = frequency;
+        n->left = NULL;
+	n->right = NULL;
+    }
+    return n;
+
+}
+
+void node_delete(Node **n) {
+    if (*n) {
+        free(*n);
+        *n = NULL;
+    }
+    return;
+}
+
+Node *node_join(Node *left, Node *right) {
+	uint64_t freq = left->frequency + right->frequency;
+	Node *new_node = node_create('$',freq);
+	new_node->left = left;
+	new_node->right = right;
+	return new_node;
+}
+void show_node(Node *n) {
+        printf("(%d, %lu)",n->symbol,n->frequency);
+}
+void node_print(Node *n) {
+	printf("-----------------------\n");
+	printf("Node: (%d, %lu)\n",n->symbol,n->frequency);
+	if(n->left) {
+		printf("left node: (%d, %lu)\n",n->left->symbol,n->left->frequency);
+	}else{
+		printf("left node: None\n");
+	}
+	if(n->right) {
+		printf("right node: (%d, %lu)\n",n->right->symbol,n->right->frequency);
+	}else{
+		printf("right node: None\n");
+	}
+}
+
---
+++ asgn6/pq.c
@@ -0,0 +1,108 @@
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "pq.h"
+#include "node.h"
+typedef struct PriorityQueue {
+    uint32_t head;
+    uint32_t tail;
+    uint32_t size;
+    uint32_t capacity;
+    Node **items;
+} PriorityQueue;
+
+PriorityQueue *pq_create(uint32_t capacity) {
+    PriorityQueue *q = (PriorityQueue *) malloc(sizeof(PriorityQueue));
+    if (q) {
+        q->head = 0;
+        q->tail = 0;
+        q->size = 0;
+        q->capacity = capacity;
+        q->items = (Node **) calloc(capacity, sizeof(Node));
+        if (!q->items) {
+            free(q);
+            q = NULL;
+        }
+    }
+    return q;
+	
+}
+
+void pq_delete(PriorityQueue **q) {
+    if (*q && (*q)->items) {
+        free((*q)->items);
+        free(*q);
+        *q = NULL;
+    }
+    return;
+}
+
+bool pq_empty(PriorityQueue *q) {
+	return q->size == 0;
+}
+
+bool pq_full(PriorityQueue *q) {
+	return q->size == q->capacity;
+}
+
+uint32_t pq_size(PriorityQueue *q) {
+	return q->size;
+}
+
+bool enqueue(PriorityQueue *q, Node *n) {
+    if (pq_full(q)) {
+        return false;
+    }
+    if(pq_empty(q)) {
+	    q->size += 1;
+	    q->items[q->tail] = n;
+	    q->tail = (q->tail + 1) % q->capacity;
+    }else{
+	    uint32_t val = q->tail;
+	    while(val != q->head) {
+		    if(q->items[val-1]->frequency > n->frequency) {
+			    q->items[val] = q->items[val-1];
+			    val--;
+		    }else{
+			    q->items[val] = n;
+			    q->size += 1;
+			    q->tail = (q->tail + 1) % q->capacity;
+			    break;
+		    }
+	    }
+	    if(val == q->head) {
+		    q->items[val] = n;
+		    q->size += 1;
+		    q->tail = (q->tail + 1) % q->capacity;
+	    }
+
+    }
+    return true;
+}
+
+bool dequeue(PriorityQueue *q, Node **n) {
+    if (pq_empty(q)) {
+        return false;
+    }
+    q->size -= 1;
+    n = &q->items[q->head];
+    q->head = (q->head + 1) % q->capacity;
+    return true;
+	
+}
+
+void pq_print(PriorityQueue *q) {
+	printf("-----------------\n");
+	printf("Queue: [");
+	for(uint32_t i = q->head; i < q->tail; i++) {
+        	printf("(%d, %lu)",q->items[i]->symbol,q->items[i]->frequency);
+		if(i != q->size-1) {
+			printf(", ");
+		}
+	}
+	printf("]\n");
+	printf("Size: %d\n",q->size);
+}
+
